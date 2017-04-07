0d48fc6bb942537f69c6f8043b12b38a506120e5
Naoya Nakazawa <naoya.n@gmail.com>
2017-04-07 15:12:17 +0900

    Use only Xvfvb

--- ubuntu-js/entry_point.sh
+++ ubuntu-js/entry_point.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-SERVERNUM=$(echo $DISPLAY | sed -r -e 's/([^:]+)?:([0-9]+)(\.[0-9]+)?/\2/')
-GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
+/usr/local/bin/xvfb start
 
-rm -f /tmp/.X*lock
-
-xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
-  java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
-  ${SE_OPTS} &
+/bin/bash
 NODE_PID=$!
 
 trap shutdown SIGTERM SIGINT
 wait $NODE_PID
-
